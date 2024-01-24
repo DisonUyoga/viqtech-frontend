d86cb7145c6a2c179a6b1b0a7788822b33af4ca0
DisonUyoga <disonobudho233@gmail.com>
2024-01-25 00:45:24 +0300

    last

--- src/App.jsx
+++ src/App.jsx
@@ -8,39 +8,69 @@ import {
 } from "react-router-dom";
 import Home from "./pages/Home";
 import Login, { action as loginAction } from "./pages/Login";
-import Signup from "./pages/Signup";
-import Cart from "./pages/Cart";
-import Shop from "./pages/Shop";
-import ProductDetails from "./pages/ProductDetails";
+
 import Layout from "./components/Layout/Layout";
-import Checkout from "./pages/Checkout";
-import ProtectedRoute from "./routers/ProtectedRoute";
+
 import { getToken, auth } from "./redux/slices/loginSlice";
 import { useSelector } from "react-redux";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { redirect } from "react-router-dom";
-import Contact from "./components/Contact";
+
 import "./components/Contact/index.scss";
+import Spinner from "./components/Spinner";
+
+const Signup = lazy(()=>import("./pages/Signup"))
+const Cart= lazy(()=>import("./pages/Cart"))
+const Shop =lazy(()=>import("./pages/Shop"))
+const ProductDetails=lazy(()=>import("./pages/ProductDetails"))
+
+const Checkout=lazy(()=>import("./pages/Checkout"))
+const ProtectedRoute=lazy(()=>import("./routers/ProtectedRoute"))
+const Contact=lazy(()=>import("./components/Contact"))
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
-      <Route path="Shop" element={<Shop />} />
-      <Route path="shop/:id" element={<ProductDetails />} />
+      <Route path="Shop" element={<Suspense fallback={<Spinner/>}>
+        
+        <Shop />
+        </Suspense>
+        } />
+      <Route path="shop/:id" element={<Suspense fallback={<Spinner/>}>
+        
+        <ProductDetails />
+        </Suspense>
+        } />
       <Route
         path="checkout"
-        element={
+        element={<Suspense fallback={<Spinner/>}>
+
+          
           <ProtectedRoute>
             <Checkout />
           </ProtectedRoute>
+        </Suspense>
         }
       />
       <Route path="login" action={loginAction} element={<Login />} />
-      <Route path="signup" element={<Signup />} />
-      <Route path="cart" element={<Cart />} />
-      <Route path="contact" element={<Contact />} />
+      <Route path="signup" element={<Suspense fallback={<Spinner/>}>
+        
+        <Signup />
+        </Suspense>
+        } />
+      <Route path="cart" element={<Suspense fallback={<Spinner/>}>
+        
+        <Cart />
+        </Suspense>
+        } />
+      <Route path="contact" element={<Suspense fallback={<Spinner/>}>
+        
+        <Contact />
+        </Suspense>
+        } />
     </Route>
   )
 );
